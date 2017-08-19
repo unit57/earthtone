b71de3b9dca9d189eb2e62dd4aa61ecdc6e95ded
Eric Sanchez <eric.q.sanchez@gmail.com>
2017-08-19 00:05:47 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(()=> {
+	console.log("connected")
+	
+	let page = $('.page')
+
+	let nav = $('nav');
+	let navButton = $('.navButton')
+	let landing = $('#landing');
+	let index = $('#index');
+	let footer = $('footer');
+
+	let enterButton = $('.enterButton')
+
+	/* Some of these display Blocks may need to be be "flex" after styling */
+/*	$(enterButton).on('click', ()=> {*/
+		/* Show Page content */
+		(function see(){
+		$(page).css('display', 'flex');
+		
+		/*Show Navbar */
+		$(nav).css('display', 'flex')
+		$(navButton).css('display', 'flex')
+		/* Show Footer */
+		$(footer).css('display', 'flex')
+		/* Remove Landing page */
+		$(landing).css('display', 'none');
+	}())
+/*	})*/
+
+	
+
+
+
+
+
+
+});
+
